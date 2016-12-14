9188632bd2f8aef1b0aec4cfe98efd2510b7a3f7
axuan2015 <xuanyouwu@163.com>
2016-12-14 16:11:45 +0800

    静态变量和静态方法的使用

--- app/src/main/java/com/xuan/asange/databindingdemo/MainActivity.java
+++ app/src/main/java/com/xuan/asange/databindingdemo/MainActivity.java
@@ -3,6 +3,7 @@ package com.xuan.asange.databindingdemo;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 
 import com.xuan.asange.databindingdemo.databinding.ActivityMainBinding;
 
@@ -15,6 +16,8 @@ public class MainActivity extends AppCompatActivity {
         //setContentView(R.layout.activity_main);
         ActivityMainBinding binding = DataBindingUtil.setContentView(
                 this, R.layout.activity_main);
+        binding.setS("这是个字符串");
+        binding.setIsMan(true);
         User user = new User("阿三哥", 27);
         binding.setUser(user);
     }
---
+++ app/src/main/java/com/xuan/asange/databindingdemo/StringUtils.java
@@ -0,0 +1,16 @@
+package com.xuan.asange.databindingdemo;
+
+/**
+ * Description
+ * Company
+ * author  youxuan  E-mail:xuanyouwu@163.com
+ * date createTime：16/12/14
+ * version
+ */
+
+public class StringUtils {
+
+    public static String getVersionName() {
+        return BuildConfig.VERSION_NAME;
+    }
+}
